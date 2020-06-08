5dad18771434ec456544432225a4024de491d0c9
Shelbie <30550643+shelbie@users.noreply.github.com>
2020-06-07 20:27:29 -0500

    added base code

--- user-card/src/App.js
+++ user-card/src/App.js
@@ -1,26 +1,46 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+class App extends React.Component {
+ state = {
+   users: [],
+ }
+
+ componentDidMount() {
+   console.log("componentDidMount Running");
+
+   axios
+   .get("https://api.github.com/users")
+   .then(res => {
+     console.log(res);
+    this.setState({ users: res.data.message })
+   })
+   .catch(err => console.log(err));
+ }
+
+ handleChanges = e => {
+   console.log(e.target.name, e.target.value);
+   this.setState({ ...this.state, })
+ }
+
+// fetchUsers = e => {
+//   e.preventDefault();
+//   axios
+//   .get(`https://api.github.com/users/`)
+// }
+
+render(){
+  console.log("rendering");
+  return(
+    <div>
+      <h1>
+      Welcome User!
+      </h1>
     </div>
-  );
+  )
+}
+
 }
 
 export default App;
